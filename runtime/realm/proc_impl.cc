StanfordLegion/legion

added support to free resources associated with a processor group; this is usefull in applications where we create/destroy lots of processors groups

runtime/realm/proc_impl.cc
@@ -99,6 +99,16 @@ namespace Realm {
       return Processor::NO_PROC;
     }
 
+    /*static*/ void Processor::free_group(const Processor& pg)
+    {
+      ProcessorGroup *grp = get_runtime()->get_procgroup_impl(pg);
+      //first we remove members and properly clean up task queues from members list
+      grp->remove_group_members();
+      
+      //return group to free list
+      get_runtime()->local_proc_group_free_list->free_entry( grp );
+    }
+  
     void Processor::get_group_members(std::vector<Processor>& members)
     {
       // if we're a plain old processor, the only member of our "group" is ourself
@@ -112,6 +122,23 @@ namespace Realm {
       ProcessorGroup *grp = get_runtime()->get_procgroup_impl(*this);
       grp->get_group_members(members);
     }
+
+    void ProcessorGroup::remove_group_members()
+    {
+      // can only be performed on owner node
+      assert(NodeID(ID(me).pgroup_owner_node()) == Network::my_node_id); // TODO check with Sean
+      for(std::vector<ProcessorImpl*>::iterator it = members.begin();
+          it != members.end();
+          it++) {
+        ProcessorImpl *m_impl = *it;
+        m_impl->remove_group(this);
+      }
+      members.clear(); //if reused we clear previous members
+      members_requested = false;
+      members_valid = false;
+      task_queue.free_gauge();
+    }
+
 
     int Processor::get_num_cores(void) const
     {
@@ -626,6 +653,10 @@ namespace Realm {
       task_queue.set_gauge(ready_task_count);
     }
 
+    void ProcessorGroup::remove_group(ProcessorGroup *group) {
+      assert(0);
+    }
+
     void ProcessorGroup::get_group_members(std::vector<Processor>& member_list)
     {
       assert(members_valid);
@@ -836,7 +867,13 @@ namespace Realm {
       amsg.commit();
     }
 
-  
+    void RemoteProcessor::remove_group(ProcessorGroup *group)
+    {
+      // not currently supported
+      assert(0);
+    }
+
+
   ////////////////////////////////////////////////////////////////////////
   //
   // class LocalTaskProcessor
@@ -884,6 +921,12 @@ namespace Realm {
   {
     // add the group's task queue to our scheduler too
     sched->add_task_queue(&group->task_queue);
+  }
+
+  void LocalTaskProcessor::remove_group(ProcessorGroup *group)
+  {
+    // remove the group's task queue from our scheduler
+    sched->remove_task_queue(&group->task_queue);
   }
 
   void LocalTaskProcessor::enqueue_task(Task *task)

runtime/realm/proc_impl.h
@@ -68,6 +68,8 @@ namespace Realm {
 
       virtual void add_to_group(ProcessorGroup *group) = 0;
 
+      virtual void remove_group(ProcessorGroup *group) = 0;
+
       virtual void register_task(Processor::TaskFuncID func_id,
 				 CodeDescriptor& codedesc,
 				 const ByteArrayRef& user_data);
@@ -144,6 +146,8 @@ namespace Realm {
 
       virtual void add_to_group(ProcessorGroup *group);
 
+      virtual void remove_group(ProcessorGroup *group);
+
       // runs an internal Realm operation on this processor
       virtual void add_internal_task(InternalTask *task);
 
@@ -215,6 +219,8 @@ namespace Realm {
 
       virtual void add_to_group(ProcessorGroup *group);
 
+      virtual void remove_group(ProcessorGroup *group);
+
       virtual void spawn_task(Processor::TaskFuncID func_id,
 			      const void *args, size_t arglen,
                               const ProfilingRequestSet &reqs,
@@ -236,12 +242,16 @@ namespace Realm {
 
       void set_group_members(const std::vector<Processor>& member_list);
 
+      void remove_group_members();
+
       void get_group_members(std::vector<Processor>& member_list);
 
       virtual void enqueue_task(Task *task);
       virtual void enqueue_tasks(Task::TaskList& tasks);
 
       virtual void add_to_group(ProcessorGroup *group);
+
+      virtual void remove_group(ProcessorGroup *group);
 
       virtual void spawn_task(Processor::TaskFuncID func_id,
 			      const void *args, size_t arglen,

ebendler/realm

runtime/realm/proc_impl.cc
@@ -99,6 +99,16 @@ namespace Realm {
       return Processor::NO_PROC;
     }
 
+    /*static*/ void Processor::free_group(const Processor& pg)
+    {
+      ProcessorGroupImpl *grp = get_runtime()->get_procgroup_impl(pg);
+      //first we remove members and properly clean up task queues from members list
+      grp->remove_group_members();
+      
+      //return group to free list
+      get_runtime()->local_proc_group_free_list->free_entry( grp );
+    }
+  
     void Processor::get_group_members(std::vector<Processor>& members)
     {
       // if we're a plain old processor, the only member of our "group" is ourself
@@ -112,6 +122,23 @@ namespace Realm {
       ProcessorGroupImpl *grp = get_runtime()->get_procgroup_impl(*this);
       grp->get_group_members(members);
     }
+
+    void ProcessorGroupImpl::remove_group_members()
+    {
+      // can only be performed on owner node
+      assert(NodeID(ID(me).pgroup_owner_node()) == Network::my_node_id); // TODO check with Sean
+      for(std::vector<ProcessorImpl*>::iterator it = members.begin();
+          it != members.end();
+          it++) {
+        ProcessorImpl *m_impl = *it;
+        m_impl->remove_group(this);
+      }
+      members.clear(); //if reused we clear previous members
+      members_requested = false;
+      members_valid = false;
+      task_queue.free_gauge();
+    }
+
 
     int Processor::get_num_cores(void) const
     {
@@ -626,6 +653,10 @@ namespace Realm {
       task_queue.set_gauge(ready_task_count);
     }
 
+    void ProcessorGroupImpl::remove_group(ProcessorGroupImpl *group) {
+      assert(0);
+    }
+
     void ProcessorGroupImpl::get_group_members(std::vector<Processor>& member_list)
     {
       assert(members_valid);
@@ -836,7 +867,13 @@ namespace Realm {
       amsg.commit();
     }
 
-  
+    void RemoteProcessor::remove_group(ProcessorGroupImpl *group)
+    {
+      // not currently supported
+      assert(0);
+    }
+
+
   ////////////////////////////////////////////////////////////////////////
   //
   // class LocalTaskProcessor
@@ -884,6 +921,12 @@ namespace Realm {
   {
     // add the group's task queue to our scheduler too
     sched->add_task_queue(&group->task_queue);
+  }
+
+  void LocalTaskProcessor::remove_group(ProcessorGroupImpl *group)
+  {
+    // remove the group's task queue from our scheduler
+    sched->remove_task_queue(&group->task_queue);
   }
 
   void LocalTaskProcessor::enqueue_task(Task *task)

runtime/realm/proc_impl.h
@@ -68,6 +68,8 @@ namespace Realm {
 
       virtual void add_to_group(ProcessorGroupImpl *group) = 0;
 
+      virtual void remove_group(ProcessorGroupImpl *group) = 0;
+
       virtual void register_task(Processor::TaskFuncID func_id,
 				 CodeDescriptor& codedesc,
 				 const ByteArrayRef& user_data);
@@ -144,6 +146,8 @@ namespace Realm {
 
       virtual void add_to_group(ProcessorGroupImpl *group);
 
+      virtual void remove_group(ProcessorGroupImpl *group);
+
       // runs an internal Realm operation on this processor
       virtual void add_internal_task(InternalTask *task);
 
@@ -215,6 +219,8 @@ namespace Realm {
 
       virtual void add_to_group(ProcessorGroupImpl *group);
 
+      virtual void remove_group(ProcessorGroupImpl *group);
+
       virtual void spawn_task(Processor::TaskFuncID func_id,
 			      const void *args, size_t arglen,
                               const ProfilingRequestSet &reqs,
@@ -236,12 +242,16 @@ namespace Realm {
 
       void set_group_members(const std::vector<Processor>& member_list);
 
+      void remove_group_members();
+
       void get_group_members(std::vector<Processor>& member_list);
 
       virtual void enqueue_task(Task *task);
       virtual void enqueue_tasks(Task::TaskList& tasks);
 
       virtual void add_to_group(ProcessorGroupImpl *group);
+
+      virtual void remove_group(ProcessorGroupImpl *group);
 
       virtual void spawn_task(Processor::TaskFuncID func_id,
 			      const void *args, size_t arglen,